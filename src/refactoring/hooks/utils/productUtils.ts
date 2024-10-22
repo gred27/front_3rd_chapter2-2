ea5b006c8f792e8d9121e56be5f68a6fdaf7ea7e
Gred <jihong.park.dev@gmail.com>
2024-10-23 01:33:45 +0900

    feat: CartPage에서 ProductItem, CartItemUnit 컴포넌트 분리

--- src/refactoring/hooks/utils/productUtils.ts
+++ src/refactoring/hooks/utils/productUtils.ts
@@ -3,3 +3,7 @@ import { CartItem, Product } from '../../../types';
 export const getRemainingStock = (product: Product, cartItem?: CartItem) => {
   return product.stock - (cartItem?.quantity || 0);
 };
+
+export const getMaxDiscount = (discounts: { quantity: number; rate: number }[]) => {
+  return discounts.reduce((max, discount) => Math.max(max, discount.rate), 0);
+};
