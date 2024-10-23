449a04226ab756bd60d50d5b82ba6b1b5fdad92b
Gred <jihong.park.dev@gmail.com>
2024-10-23 22:43:33 +0900

    feat: AddNewProductSection, useAddProduct 분리

---
+++ src/refactoring/hooks/useAddProduct.ts
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { Product } from '../../types';
+
+type UpdatedKeyType = keyof Omit<Product, 'id' | 'discounts'>;
+
+interface IUseAddProductProps {
+  onProductAdd: (newProduct: Product) => void;
+}
+
+export const useAddProduct = ({ onProductAdd }: IUseAddProductProps) => {
+  const [showNewProductForm, setShowNewProductForm] = useState(false);
+  const [newProduct, setNewProduct] = useState<Omit<Product, 'id'>>({
+    name: '',
+    price: 0,
+    stock: 0,
+    discounts: [],
+  });
+
+  const handleAddNewProduct = () => {
+    const productWithId = { ...newProduct, id: Date.now().toString() };
+    onProductAdd(productWithId);
+    setNewProduct({
+      name: '',
+      price: 0,
+      stock: 0,
+      discounts: [],
+    });
+    setShowNewProductForm(false);
+  };
+
+  const handleToggleShowNewProductBtn = () => setShowNewProductForm(!showNewProductForm);
+
+  const handleChangeProductForm =
+    (updatedKey: UpdatedKeyType) => (e: React.ChangeEvent<HTMLInputElement>) =>
+      setNewProduct({ ...newProduct, [updatedKey]: getUpdatedValue(updatedKey, e.target.value) });
+
+  const getUpdatedValue = (key: string, value: string) => {
+    return key === 'name' ? value : parseInt(value);
+  };
+
+  return {
+    showNewProductForm,
+    newProduct,
+    handleToggleShowNewProductBtn,
+    handleAddNewProduct,
+    handleChangeProductForm,
+  };
+};
