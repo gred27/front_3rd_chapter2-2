76366ffa5c090b6a27a3c65ff7fb46e377592da4
Gred <jihong.park.dev@gmail.com>
2024-10-25 00:29:45 +0900

    feat: productContext, couponContext 생성

---
+++ src/refactoring/context/CouponContext.tsx
@@ -0,0 +1,18 @@
+import { useMemo } from 'react';
+import { useCoupons } from '../hooks';
+import { createContext } from '../lib/create-context';
+import { initialCoupons } from '../mock';
+
+type ICouponContext = ReturnType<typeof useCoupons>;
+
+const [CouponContextProvider, useCouponContext] = createContext<ICouponContext>({
+  name: 'Coupon',
+});
+
+const CouponProvider = ({ children }: { children: React.ReactNode }) => {
+  const { coupons, addCoupon } = useCoupons(initialCoupons);
+  const contextValue = useMemo(() => ({ coupons, addCoupon }), [addCoupon, coupons]);
+  return <CouponContextProvider value={contextValue}>{children}</CouponContextProvider>;
+};
+
+export { CouponProvider, useCouponContext };
---
+++ src/refactoring/context/ProductContext.tsx
@@ -0,0 +1,21 @@
+import { useMemo } from 'react';
+import { useProducts } from '../hooks';
+import { createContext } from '../lib/create-context';
+import { initialProducts } from '../mock';
+
+type IProductContext = ReturnType<typeof useProducts>;
+
+const [ProductContextProvider, useProductContext] = createContext<IProductContext>({
+  name: 'Product',
+});
+
+const ProductProvider = ({ children }: { children: React.ReactNode }) => {
+  const { products, updateProduct, addProduct } = useProducts(initialProducts);
+  const contextValue = useMemo(
+    () => ({ products, updateProduct, addProduct }),
+    [addProduct, products, updateProduct],
+  );
+  return <ProductContextProvider value={contextValue}>{children}</ProductContextProvider>;
+};
+
+export { ProductProvider, useProductContext };
